e6f6c2cae848938ec0b8c8246760993050fc3fdd
Peter Joshua <peterjoshua828@gmail.com>
2020-08-25 09:14:28 +0100

    Updates

---
+++ precache-manifest.ccf15ab308ae409e84be71b7d6631ca3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0d3431bc3ad9ef441dc60eb2103851",
+    "url": "/react-genesis/index.html"
+  },
+  {
+    "revision": "c3218c8ffd43696b553c",
+    "url": "/react-genesis/static/css/main.6b55c9fa.chunk.css"
+  },
+  {
+    "revision": "d82f3cc67485b1ced188",
+    "url": "/react-genesis/static/js/2.3ed857da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-genesis/static/js/2.3ed857da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3218c8ffd43696b553c",
+    "url": "/react-genesis/static/js/main.b6228d17.chunk.js"
+  },
+  {
+    "revision": "29a3ac997440cdf5012c",
+    "url": "/react-genesis/static/js/runtime-main.da15a77d.js"
+  }
+]);
